c1f03353be9ec8440868efbf1fc5de81abcc56cc
James Conlin <jacon.dev@outlook.com>
2021-12-12 11:43:22 +0000

    Part 1 of Day 2 completed - adding structure for part 2

---
+++ Day1/Day2/Day2Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Day2
+{
+    [TestFixture]
+    public class Day2Tests
+    {
+        private readonly string[] _inputDirections = { "forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2" };
+        private const string CodeWarsInputFilePath = @"../../../Day2Input";
+
+        [Test]
+        public void TestThatSubmarineMovesAsExpected()
+        {
+            var response = SubmarineControls.MoveSubmarine(_inputDirections);
+            
+            Assert.AreEqual(150, response);
+        }
+
+        [Test]
+        public void TestThatSubmarineMovesAsExpectedUsingCodeWarsInput()
+        {
+            var inputDirections = File.ReadAllLines(CodeWarsInputFilePath);
+            
+            var response = SubmarineControls.MoveSubmarine(inputDirections);
+            
+            Assert.AreEqual(1427868, response);
+        }
+
+        [Test]
+        public void TestThatAimingTheSubmarineWorksAsExpected()
+        {
+            var response = SubmarineControls.AimSubmarine(_inputDirections);
+            
+            Assert.AreEqual(900, response);
+        }
+    }
+}
---
+++ Day1/Day2/SubmarineControls.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace Day2
+{
+    public class SubmarineControls
+    {
+        public static double MoveSubmarine(string[] inputDirections)
+        {
+            var depth = 0;
+            var horizontalPosition = 0;
+
+            foreach (var direction in inputDirections)
+            {
+                var movement = direction.Split(" ").First();
+                int.TryParse(direction.Split(" ").Last(), out var distance);
+                switch (movement)
+                {
+                    case "forward" : horizontalPosition += distance;
+                        break;
+                    case "up" : depth -= distance ;
+                        break;
+                    case "down" : depth += distance;
+                        break;
+                }
+            }
+
+            return depth * horizontalPosition;
+        }
+
+        public static double AimSubmarine(string[] inputDirections)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
