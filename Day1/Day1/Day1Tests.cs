c64a8af550765db58a0edc67c47dea33a4dcef35
James Conlin <jacon.dev@outlook.com>
2021-12-12 10:36:21 +0000

    Completed Day 1 of the Code Wars Advent of Code!

---
+++ Day1/Day1/Day1Tests.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Day1
+{
+    [TestFixture]
+    public class Day1Tests
+    {
+        private readonly int[] _inputArray = { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+        private const string CodeWarsInputFilePath = @"../../../Day1Input";
+        
+        [Test]
+        public void TestThatCountOfIncreasedDepthIsAccurateFromSonarSweep()
+        {
+            var depthCheckerResponse = DepthChecker.SonarSweep(_inputArray);
+            
+            Assert.AreEqual(7, depthCheckerResponse);
+        }
+        
+        [Test]
+        public void TestInputAndResponseFromCodeWarsFileIntoSonarSweep()
+        {
+            var input = File.ReadAllLines(CodeWarsInputFilePath).Select(int.Parse).ToArray();
+            
+            var depthCheckerResponse = DepthChecker.SonarSweep(input);
+            Assert.AreEqual(1713, depthCheckerResponse);
+        }
+
+        [Test]
+        public void TestThatSumOfSlidingWindowIsExpectedFromSummedSonarSweep()
+        {
+            var summedResponse = DepthChecker.SummedSonarSweep(_inputArray);
+            
+            Assert.AreEqual(5, summedResponse);
+        }
+
+        [Test]
+        public void TestThatSumOfCodeWarsInputIsCorrectFromSummedSonarSweep()
+        {
+            var input = File.ReadAllLines(CodeWarsInputFilePath).Select(int.Parse).ToArray();
+            
+            var summedResponse = DepthChecker.SummedSonarSweep(input);
+            Assert.AreEqual(1734, summedResponse);
+        }
+    }
+}
---
+++ Day1/Day1/DepthChecker.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace Day1
+{
+    public class DepthChecker
+    {
+        public static int SonarSweep(int[] inputArray)
+        {
+            var count = 0;
+            var previousInput = inputArray.First();
+            foreach (var depth in inputArray)
+            {
+                if (depth > previousInput) count++;
+                previousInput = depth;
+            }
+
+            return count;
+        }
+
+        public static int SummedSonarSweep(int[] inputArray)
+        {
+            var count = 0;
+            var previousSum = inputArray.Take(3).Sum(x => x);
+            
+            for (var i = 3; i < inputArray.Length; i++)
+            {
+                var sum = inputArray[i] + inputArray[i - 1] + inputArray[i - 2];
+                if (sum > previousSum) count++;
+                previousSum = sum;
+            }
+
+            return count;
+        }
+    }
+}
